fc9ad769072bcfd5ee15fab2931b485007219788
dengwenyao <272987067@qq.com>
2021-10-07 22:48:56 +0800

    add window class, exception, ico, and so on

--- d3dStudy/d3dStudy/WinMain.cpp
+++ d3dStudy/d3dStudy/WinMain.cpp
@@ -1,36 +1,37 @@
-#include <Windows.h>
+#include"Window.h"
 
 int CALLBACK WinMain(
     HINSTANCE hInstance,
     HINSTANCE hPrevInstance,
     LPSTR     lpCmdLine,
     int       nCmdShow) {
-    const auto pClassName = "hw3dbutts";
-    // register window class
-    WNDCLASSEX wc = { 0 };
-    wc.cbSize = sizeof(wc);
-    wc.style = CS_OWNDC;
-    wc.lpfnWndProc = DefWindowProc;
-    wc.cbClsExtra = 0;
-    wc.cbWndExtra = 0;
-    wc.hInstance = hInstance;
-    wc.hIcon = nullptr;
-    wc.hCursor = nullptr;
-    wc.hbrBackground = nullptr;
-    wc.lpszMenuName = nullptr;
-    wc.lpszClassName = pClassName;
-    wc.hIconSm = nullptr;
-    RegisterClassEx(&wc);
-    // create window instance
-    HWND hWnd = CreateWindowEx(
-        0, pClassName,
-        "Happy Hard Window",
-        WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-        200, 200, 640, 480,
-        nullptr, nullptr, hInstance, nullptr
-    );
-    // show the damn window
-    ShowWindow(hWnd, SW_SHOW);
-    while (true);
-    return 0;
+    try {
+        Window wnd(800, 300, "Donkey Fart Box");
+
+        MSG msg;
+        BOOL gResult;
+        while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0) {
+            // TranslateMessage will post auxilliary WM_CHAR messages from key msgs
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        // check if GetMessage call itself borked
+        if (gResult == -1) {
+            throw CHWND_LAST_EXCEPT();
+        }
+
+        // wParam here is the value passed to PostQuitMessage
+        return msg.wParam;
+    }
+    catch (const ChiliException& e) {
+        MessageBox(nullptr, e.what(), e.GetType(), MB_OK | MB_ICONEXCLAMATION);
+    }
+    catch (const std::exception& e) {
+        MessageBox(nullptr, e.what(), "Standard Exception", MB_OK | MB_ICONEXCLAMATION);
+    }
+    catch (...) {
+        MessageBox(nullptr, "No details available", "Unknown Exception", MB_OK | MB_ICONEXCLAMATION);
+    }
+    return -1;
 }
